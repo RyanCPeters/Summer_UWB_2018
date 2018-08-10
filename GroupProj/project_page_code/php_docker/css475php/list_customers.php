42320fb82440594742cad734a15c4658576ab467
Ryan Peters <petersryan84@gmail.com>
2018-08-10 14:12:06 -0700

    incremental update to code for setting up the phpMyAdmin uw-hosted site

---
+++ GroupProj/project_page_code/php_docker/css475php/list_customers.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MKochanski
+ * Date: 7/24/2018
+ * Time: 3:07 PM
+ */
+require_once 'config.inc.php';
+
+?>
+<html>
+<head>
+    <title>Sample PHP Database Program</title>
+    <link rel="stylesheet" href="styling/base.css">
+</head>
+<body>
+<?php
+require_once 'header.inc.php';
+?>
+<div>
+    <h2>Customer List</h2>
+    <?php
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database, $port);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT CustomerNumber,CustomerName FROM Customer ORDER BY CustomerName";
+    $stmt = $conn->stmt_init();
+    if (!$stmt->prepare($sql)) {
+        echo "failed to prepare";
+    }
+    else {
+        $stmt->execute();
+        $stmt->bind_result($customerNumber,$customerName);
+        echo "<ul>";
+        while ($stmt->fetch()) {
+            echo '<li><a href="show_customer.php?id='  . $customerNumber . '">' . $customerName . '</a></li>';
+        }
+        echo "</ul>";
+    }
+
+    $conn->close();
+
+    ?>
+</div>
+</body>
+</html>
---
+++ GroupProj/project_page_code/php_docker/css475php/list_products.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MKochanski
+ * Date: 7/24/2018
+ * Time: 3:07 PM
+ */
+require_once 'config.inc.php';
+
+?>
+<html>
+<head>
+    <title>Sample PHP Database Program</title>
+    <link rel="stylesheet" href="styling/base.css">
+</head>
+<body>
+<?php
+require_once 'header.inc.php';
+?>
+<div>
+    <h2>Product Catalog</h2>
+    <?php
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database, $port);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT ItemNumber,ItemDescription,CategoryCode FROM catalogitem";
+    $stmt = $conn->stmt_init();
+    if (!$stmt->prepare($sql)) {
+        echo "failed to prepare";
+    }
+    else {
+        $stmt->execute();
+        $stmt->bind_result($itemNumber,$description, $category_code);
+        while ($stmt->fetch()) {
+            echo "<p>" . $description . "</p>";
+        }
+    }
+
+    $conn->close();
+
+    ?>
+</div>
+</body>
+</html>
